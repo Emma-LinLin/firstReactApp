0674547b7b5b23aca9d51f8c4ba8246d09ab7252
Emma-LinLin <74680845+Emma-LinLin@users.noreply.github.com>
2021-09-20 16:32:39 +0200

    Added logic for shopview

--- src/views/shopview/ShopView.js
+++ src/views/shopview/ShopView.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import spinner from "../../shared/images/spinner.gif"
 import "./ShopView.css"
+import axios from 'axios';
 
 export const ShopView = () =>{
+    const[responseData, setResponseData] = useState()
+    const[count, setCount] = useState(1)
+    const API_URL = "https://swapi.dev/api/planets/" + count;
+
+    const fetchData = async () =>{
+        try{
+            const response = await axios.get(API_URL)
+            setResponseData(response)
+            setCount(count+1)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const displayData = () => {
+        if(responseData && responseData.data){
+            return (
+                <div>
+                    <h3>Available flight found to</h3>
+                    <hr/>
+                    <h3>Name: {responseData.data.name}</h3>
+                    <h3>Diameter: {responseData.data.diameter}</h3>
+                    <h3>Climate: {responseData.data.climate}</h3>
+                    <h3>Terrain: {responseData.data.terrain}</h3>
+                    <br/>
+                </div>
+            )
+        }
+        return(
+            <div>
+                {showLoadingIfNoAPICall()}
+            </div>
+        )
+    }
 
     const showLoadingIfNoAPICall = () =>{
         return(
@@ -16,7 +51,8 @@ export const ShopView = () =>{
     return(
         <div className="shopview">
             <h1>Book a flight</h1>
-            {showLoadingIfNoAPICall()}
+            <button onClick={() => fetchData()}>Check for nearest department</button>
+            {displayData()}
         </div>
     )
 };
